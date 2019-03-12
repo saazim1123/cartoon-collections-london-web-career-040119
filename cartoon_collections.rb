8f166f208d540d773ca4e6946d3b5dad122251e0
Adnan Azim <saazim1123@gmail.com>
2019-03-12 01:16:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,18 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
   # Your code here
+  dwarves.each_with_index do |dwarf, index|
+    index += 1
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)
   # Your code here
+  powers = planeteer_calls.map(&:capitalize)
+  powers.map do |power|
+    puts "#{power}!"
+  end
+  
 end
 
 def long_planeteer_calls# code an argument here
